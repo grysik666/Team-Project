68831241b5d4623e32fb254737a5885e3caae4a9
bezubik jakub <bezubikj@p21812.MINI>
2022-10-24 11:19:52 +0200

    add class

--- Source/Algorithm.py
+++ Source/Algorithm.py
@@ -1,5 +1,10 @@
 import math
 
-def distance(centre,house):
-    return math.sqrt((centre[0]-house[0])^2+(centre[1]-house[1])^2)
+class Data:
+    def __init__(self, centre, house):
+        self.centre = centre
+        self.house = house
+
+    def distance(self):
+        return math.sqrt((self.centre[0]-self.house[0])^2+(self.centre[1]-self.house[1])^2)
 
