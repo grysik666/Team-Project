31ed5304fe3ae8233f929cfa706507696708600a
Janek Gryczka <janekgryczka@gmail.com>
2022-11-12 13:30:23 +0100

    add new functions

--- Source/Algorithm.py
+++ Source/Algorithm.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-class Data:
+class Hungarian_Algorithm:
 
     def __init__(self, centre, capacity, house):
         """[Constructor]
@@ -36,11 +36,11 @@ class Data:
                 .replace('\n', '')
                 .split(' '))))
 
-        for j in range(0, len(temp[0])-1, 2):
+        for j in range(0, len(temp[0]) - 1, 2):
             centre.append([temp[0][j],temp[0][j+1]])
         for j in range(len(temp[1])):
             capacity.append(temp[1][j])
-        for j in range(0, len(temp[2])-1, 2):
+        for j in range(0, len(temp[2]) - 1, 2):
             house.append([temp[2][j],temp[2][j+1]])
 
         self.centre = centre
@@ -79,16 +79,16 @@ class Data:
         """
         A = self.calculate_adjency_matrix()
         NoOfHouses = int(sum(self.capacity))
-        y = np.zeros((NoOfHouses * 2)) # najpierw centra * pojemnosc, potem domy
+        Y = np.zeros((NoOfHouses * 2)) # najpierw centra * pojemnosc, potem domy
         column = 0
-        for i in range(NoOfHouses, len(y), 1):
+        for i in range(NoOfHouses, len(Y), 1):
             minimum = A[0][column]
             for row in range(A.shape[0]):
                 if A[row][column] < minimum:
                     minimum = A[row][column]
-            y[i] = minimum
+            Y[i] = minimum
             column += 1
-        return y
+        return Y
     
     def generate_bipartite_graph(self):
         NoOfHouses = int(len(self.house))
@@ -100,42 +100,99 @@ class Data:
                 G[j + NoOfCentres, i] = self.distance(i,j)
         return G
     
-    def intersection(list1, list2):
-        return [value for value in list1 if (value in list2)]
-
-    def subtraction(list1,list2):
-        return[value for value in list1 if (value not in list2)]
-
-    def delete_vertex_from_R(L,x):
-        for i in range(len(L)):
-            if L[i]==x:
-                return L[:i]+L[i+1:]
-        return L
-    
-    def update_R(self, R, M, Centre: bool):
-        if Centre:
+    def update_R(self, R, M, IsCentre: bool):
+        # copy()
+        if IsCentre:
             i = 0
         else:
             i = 1
         for pairs in M:
             if pairs[i] in R:
-                R = self.delete_vertex_from_R(R,pairs[0])
+                R = delete_element_from_list(R, pairs[0])
             if R == None:
                 return []
         return R
     
-    def hungarian_algorithm(self):
+    def reverse_path(self, G_y, R_C, R_H):
+        # copy()
+        NoOfCentres = int(len(self.centre))        
+        path = BFS(G_y,R_C,R_H) # parzyste to centra
+        
+        for i in range(len(path) - 1):
+            temp = G_y[path[i]][path[i + 1]]
+            G_y[path[i]][path[i + 1] + NoOfCentres] = 0
+            G_y[path[i + 1] + NoOfCentres][path[i]] = temp
+        return G_y
+
+    def update_M(self, M, G_y):
+        # TODO
+        return M
+    
+    def update_Z(self, Z, G_y, R_C, R_H):
+        # TODO
+        return Z
+    
+    def update_Gy(self, G_y, G, y):
+        # TODO
+        return G_y    
+    
+    def calculate_delta(self, delta, Z, y):
+        # TODO
+        return delta
+        
+    def main_algorithm(self):
         M = []
+        G = self.generate_bipartite_graph()
         NoOfHouses = int(len(self.house))
         NoOfCentres = int(len(self.centre))
+        Y = self.init_potential()
         G_y = np.zeros((NoOfCentres + NoOfHouses, NoOfCentres + NoOfHouses))
-        R_C = list(range(NoOfCentres)) #wierzcholki niepokryteprzez M
-        R_H = list(range(NoOfHouses)) #wierzcholki niepokryteprzez M
+        R_C = list(range(NoOfCentres)) # wierzcholki niepokryte przez M
+        R_H = list(range(NoOfHouses)) # wierzcholki niepokryte przez M
         Z = list(range(NoOfHouses))
+        Centre_iterator = list(range(len(self.centre))) # indeksy studni
+        House_iterator = list(range(len(self.house))) # indeksy domow
+        
+        
         while len(M) < (NoOfHouses):
             
-            if len(self.intersection(R_H, Z))!=0:
-                # TODO
-                #odwróć skierowanie ścieżki w Gy wychodzącej z RS i wchodzącej do RD .
+            if len(intersection(R_H, Z)) != 0:
+                G_y = self.reverse_path(G_y, R_C, R_H)
+            else:
+                delta = self.calculate_delta
+                Z_Intersection_C = intersection(Z, Centre_iterator)
+                Z_Intersection_H = intersection(Z, range(House_iterator))
+
+                for i in Z_Intersection_C: # dodawanie odejmowanie delty
+                    Y[i] += delta
+                for j in Z_Intersection_H:
+                    Y[j] -= delta
+            
+                G_y = self.update_Gy(G, Y, G_y) # dodaje ciasne i usuwam nieciasne krawedzie z Gy
+
+                M = self.update_M(G_y)
+                R_C = self.update_R(R_C, M, True)
+                R_H = self.update_R(R_C, M, False)
+                Z = self.update_Z(Z, G_y, R_C, R_H)
+                
+        return M
                 
                 
+def intersection(list1, list2):
+    return [value for value in list1 if (value in list2)]
+
+def subtraction(list1, list2):
+    return[value for value in list1 if (value not in list2)]
+
+def delete_element_from_list(L, x):
+    for i in range(len(L)):
+        if L[i] == x:
+            return L[:i] + L[i + 1:]
+    return L
+
+def BFS(G, A, B):
+    '''A,B podzbiory wierzcholkow G
+    zwraca sciezke z wierzcholka z A  do wierzcholka z B o ile taka istnieje''' 
+    # TODO
+    return []
+
