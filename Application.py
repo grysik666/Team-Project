09e15f769013a5d0ec2aa13d4631f74f7687d825
bezuu <63921655+bezuu@users.noreply.github.com>
2023-01-15 15:34:18 +0100

    add user generated data function

--- Application.py
+++ Application.py
@@ -50,7 +50,7 @@ class Application(QWidget):
         layoutT.addLayout(layoutH, 0, 0, 1, 3)
         exit_Button.clicked.connect(self.finish)
         random_values.clicked.connect(self.random_values)
-        #random_values_by_user.clicked.connect(self.random_values_generated_by_user)
+        random_values_by_user.clicked.connect(self.values_generated_by_user)
         own_values.clicked.connect(self.open_file)
           
     def open_file(self):
@@ -87,6 +87,38 @@ class Application(QWidget):
         G = Graph.Graphs()
         G.plot_graph(Centre, Capacity, House, M)
 
+    def values_generated_by_user(self):
+        Centre, done1 = QInputDialog.getText(
+            self, 'Współrzędne centrów', 'Podaj współrzędne centrów')
+        Capacity, done2 = QInputDialog.getText(
+            self, 'Limity centrów', 'Podaj limity centrów')
+        House, done3 = QInputDialog.getText(
+            self, 'Współrzędne domów', 'Podaj współrzędne domów')
+
+        Centre = [float(i) for i in Centre.split(' ')]
+        Capacity = [int(i) for i in Capacity.split(' ')]
+        House = [float(i) for i in House.split(' ')]
+        centre = []
+        capacity = []
+        house = []
+        for j in range(0, len(Centre) - 1, 2):
+            centre.append([Centre[j], Centre[j + 1]])
+        for j in range(len(Capacity)):
+            capacity.append(Capacity[j])
+        for j in range(0, len(House) - 1, 2):
+            house.append([House[j], House[j + 1]])
+
+        G = Algorithm.Hungarian_Algorithm(centre, capacity, house)
+        M, Result, StringResult = G.main_algorithm(True)
+        StrResult = 'Łączna odległość pomiędzy połączonymi centrami i domami wynosi ' + str(round(Result, 3)) + ' km'
+        Box = ScrollMessageBox(1, "Wynik", StrResult + '\n' + StringResult, QMessageBox.Save | QMessageBox.Ok)
+        ret = Box.exec_()
+        if ret == QMessageBox.Save:
+            with open('Results.txt', 'w') as f:
+                f.write(StrResult + '\n' + StringResult)
+        G = Graph.Graphs()
+        G.plot_graph(centre, capacity, house, M)
+        
     def finish(self):
         self.close()
         sys.exit(app.exec_())
